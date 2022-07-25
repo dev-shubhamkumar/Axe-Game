6f251b55596af6dcc7a62850361de7c23023dfa9
Shubham Kumar <developer.shubham07@gmail.com>
2022-07-25 23:31:09 +0530

    Adding Using Braced Initialization

--- main.cpp
+++ main.cpp
@@ -6,10 +6,13 @@ int main()
     int width = 350;
     int height = 200;
 
+    //Using Braced Initialization
+    bool checker{false};
+
     //Creating a window using raylib
     InitWindow(width, height, "Axe Game Window");
     
-    while (WindowShouldClose() == false)
+    while (WindowShouldClose() == checker)
     {
         BeginDrawing();
         ClearBackground(RED);
