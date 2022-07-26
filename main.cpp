c08493de45e6bc7e2bda935d2b091216102926b2
Shubham Kumar <developer.shubham07@gmail.com>
2022-07-26 20:50:24 +0530

    Adding functionality to move circle Up and Right and Setting Target FPS

--- main.cpp
+++ main.cpp
@@ -3,21 +3,37 @@
 
 int main()
 {
+    // Window dimension
     int width = 350;
     int height = 200;
 
     //Using Braced Initialization
     bool checker{false};
 
+    //Circle Coordinates
+    int circle_x{175};
+    int circle_y{100};
+
     //Creating a window using raylib
     InitWindow(width, height, "Axe Game Window");
     
+    SetTargetFPS(60);
     while (WindowShouldClose() == checker)
     {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
 
-        DrawCircle(175, 100, 25, BLUE);
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D))
+        {
+            circle_x = circle_x + 10;
+        }
+        else if (IsKeyDown(KEY_A))
+        {
+            circle_y = circle_y - 10;
+        }
+        
 
         EndDrawing();
     }
