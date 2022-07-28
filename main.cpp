1023b8b8b83429a2a1b4213aecd0e52d2e48b60e
Shubham Kumar <developer.shubham07@gmail.com>
2022-07-28 21:23:20 +0530

    adding collision logic and Finishing the Game

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main()
     bool checker{false};
 
     //Circle Coordinates
-    int circle_x{width/2};
+    int circle_x{width/2 - 200};
     int circle_y{height/2};
     int circle_radius{25};
     int circle_radius_inner{20};
@@ -34,7 +34,10 @@ int main()
     int direction{10};
 
     //Logic for collision
-    bool collision_with_axe{true};
+    bool collision_with_axe =   (b_axe_y >= u_circle_y) && 
+                                (u_axe_y <= b_circle_y) && 
+                                (l_axe_x <= r_circle_x) && 
+                                (r_axe_x >= l_circle_x);
 
     //Creating a window using raylib
     InitWindow(width, height, "Axe Game Window");
@@ -47,10 +50,27 @@ int main()
 
         if (collision_with_axe)
         {
-            DrawText("Game Over", 400, 200, 20, RED);
+            DrawText("Game Over", 400, 200, 40, RED);
         }
         else 
         {
+            //Game Logic Begins
+
+            //Updating Edges at every step
+            l_circle_x = circle_x - circle_radius;
+            r_circle_x = circle_x + circle_radius;
+            u_circle_y = circle_y - circle_radius;
+            b_circle_y = circle_y + circle_radius;
+            l_axe_x = axe_x;
+            r_axe_x = axe_x + axe_length;
+            u_axe_y = axe_y;
+            b_axe_y = axe_y + axe_length;
+
+            collision_with_axe =    (b_axe_y >= u_circle_y) && 
+                                    (u_axe_y <= b_circle_y) && 
+                                    (l_axe_x <= r_circle_x) && 
+                                    (r_axe_x >= l_circle_x);
+
             DrawCircle(circle_x, circle_y, circle_radius, BLUE);
             DrawCircle(circle_x, circle_y, circle_radius_inner, WHITE);
 
@@ -69,14 +89,16 @@ int main()
             {
                 circle_x = circle_x - 10;
             }
-            // else if (IsKeyDown(KEY_W) && circle_y > 0)
-            // {
-            //     circle_y = circle_y - 10;
-            // }
-            // else if (IsKeyDown(KEY_X) && circle_y < height)
-            // {
-            //     circle_y = circle_y + 10;
-            // }
+            else if (IsKeyDown(KEY_W) && circle_y > 0)
+            {
+                circle_y = circle_y - 10;
+            }
+            else if (IsKeyDown(KEY_X) && circle_y < height)
+            {
+                circle_y = circle_y + 10;
+            }
+
+            //Game Logic Ends
         }
         EndDrawing();
     }
